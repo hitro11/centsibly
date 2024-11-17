5a5da8d1e9b695d6f0228339abfbf74d39ad34f0
potatopie32 <rohitnazareth07@gmail.com>
2024-11-17 12:08:34 -0500

    backend refactor

--- server/src/api/routes/auth.ts
+++ server/src/api/routes/auth.ts
@@ -1,24 +1,19 @@
 import { logger } from '../../config/logger.js';
-import {
-  getAccessTokenController as getTokensController,
-  getJwtController,
-  getOauthRedirectUriController,
-  refreshAccessTokenController as refreshTokensController,
-} from '../controllers/auth.js';
+import * as authController from '../controllers/auth.js';
 import { Router, Request, Response } from 'express';
 
 const loginRoutes = Router();
 
 loginRoutes.get('/get-oauth-code', async (req: Request, res: Response) => {
-  const url = await getOauthRedirectUriController();
+  const url = await authController.getOauthRedirectUri();
   res.json(url);
 });
 
 loginRoutes.get('/get-tokens', async (req: Request, res: Response) => {
   const code = req.query.code?.toString() ?? '';
   const state = req.query.state?.toString() ?? '';
-  const accessToken = await getTokensController(code, state);
-  const { authToken, username } = await getJwtController(
+  const accessToken = await authController.getAccessToken(code, state);
+  const { authToken, username } = await authController.getAuthToken(
     accessToken.access_token
   );
   res.json({ accessToken, authToken, username });
@@ -27,8 +22,8 @@ loginRoutes.get('/get-tokens', async (req: Request, res: Response) => {
 loginRoutes.post('/refresh-tokens', async (req: Request, res: Response) => {
   const refreshToken = req.body.refreshToken ?? '';
   const usernameQuery = req.body.username ?? '';
-  const { accessToken } = await refreshTokensController(refreshToken);
-  const { authToken, username } = await getJwtController(
+  const { accessToken } = await authController.refreshAccessToken(refreshToken);
+  const { authToken, username } = await authController.getAuthToken(
     accessToken.access_token,
     usernameQuery
   );
@@ -37,7 +32,7 @@ loginRoutes.post('/refresh-tokens', async (req: Request, res: Response) => {
 
 loginRoutes.post('/get-jwt', async (req: Request, res: Response) => {
   const accessToken = req.body?.accessToken ?? '';
-  const authToken = await getJwtController(accessToken);
+  const authToken = await authController.getAuthToken(accessToken);
   res.json(authToken ?? null);
 });
 

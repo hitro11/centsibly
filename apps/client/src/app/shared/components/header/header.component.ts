ff90b704d7461637fe51c5839c6faf33232a06c0
Rohit Nazareth <rohitnazareth07@gmail.com>
2024-12-17 19:29:30 -0500

    move dark mode toggle to header
    - move dark mode toggle from profile menu to header
    - replace toggle switch and text with moon icon for dark mode and sun
    icon for light mode

--- apps/client/src/app/shared/components/header/header.component.ts
+++ apps/client/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,28 @@
+import { BrnMenuTriggerDirective } from '@spartan-ng/ui-menu-brain';
 import { AuthenticationService } from '../../../auth/services/authentication.service';
 import { Component, effect, inject, Signal } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
 import { HeaderProfileDropdownComponent } from './header-profile-dropdown/header-profile-dropdown.component';
 import { Router } from '@angular/router';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
-import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
-
+import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
+import { lucideMoon, lucideSun } from '@ng-icons/lucide';
+import {
+    HlmMenuComponent,
+    HlmMenuGroupComponent,
+    HlmMenuItemCheckboxDirective,
+    HlmMenuItemCheckComponent,
+    HlmMenuItemDirective,
+    HlmMenuItemIconDirective,
+    HlmMenuItemRadioComponent,
+    HlmMenuItemRadioDirective,
+    HlmMenuItemSubIndicatorComponent,
+    HlmMenuLabelComponent,
+    HlmMenuSeparatorComponent,
+    HlmMenuShortcutComponent,
+    HlmSubMenuComponent,
+    HlmMenuModule,
+} from '@spartan-ng/ui-menu-helm';
 @Component({
     selector: 'app-header',
     standalone: true,
@@ -13,26 +30,35 @@ import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
         HeaderProfileDropdownComponent,
         HlmButtonDirective,
         HlmIconComponent,
+        HlmMenuComponent,
+        HlmSubMenuComponent,
+        HlmMenuItemDirective,
+        HlmMenuItemSubIndicatorComponent,
+        HlmMenuLabelComponent,
+        HlmMenuShortcutComponent,
+        HlmMenuSeparatorComponent,
+        HlmMenuItemIconDirective,
+        HlmMenuItemCheckComponent,
+        HlmMenuItemRadioComponent,
+        HlmMenuGroupComponent,
+        HlmMenuItemRadioDirective,
+        HlmMenuItemCheckboxDirective,
+        HlmMenuModule,
+        BrnMenuTriggerDirective,
     ],
+    providers: [provideIcons({ lucideMoon, lucideSun })],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
     logoPath = './img/logo.png';
     themeService: ThemeService = inject(ThemeService);
+    theme = this.themeService.getTheme();
     authService: AuthenticationService = inject(AuthenticationService);
     isUserLoggedIn: Signal<boolean> = this.authService.isUserLoggedIn();
-    theme = this.themeService.getTheme();
     router = inject(Router);
 
-    constructor() {
-        // effect(() => {
-        //   this.logoPath =
-        //     this.theme() === 'dark'
-        //       ? './img/logo-dark.png'
-        //       : './img/logo-light.png';
-        // });
-    }
+    constructor() {}
 
     async signIn() {
         this.router.navigate(['/auth']);
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -4,19 +4,19 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
-  darkMode: 'class',
-  // content: ['./src/**/*.{html,ts}', './src/libs/**/*.{html,ts}'],
-  content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'primary-2': '#1f3323',
-      },
+    presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
+    darkMode: 'class',
+    // content: ['./src/**/*.{html,ts}', './src/libs/**/*.{html,ts}'],
+    content: [
+        join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+        ...createGlobPatternsForDependencies(__dirname),
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary-2': '#1f3323',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };

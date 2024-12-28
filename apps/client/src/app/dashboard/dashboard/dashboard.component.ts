317b1d0da5239f5361ea2315abc7268cbfbd6444
Rohit Nazareth <rohitnazareth07@gmail.com>
2024-12-28 02:00:52 -0500

    add banner to dashboard if user has no budgets
    add banner to dashboard if user does not have a current budget. The banner

--- apps/client/src/app/dashboard/dashboard/dashboard.component.ts
+++ apps/client/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,12 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import {
+    Component,
+    effect,
+    ElementRef,
+    inject,
+    OnInit,
+    signal,
+    ViewChild,
+} from '@angular/core';
 import { BudgetService } from '../../setup-account/services/budget/budget.service';
 import { Chart, ChartItem } from 'chart.js/auto';
 import {
@@ -9,7 +17,11 @@ import {
 import { ThemeService } from '../../shared/services/theme.service';
 import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 import { provideIcons } from '@ng-icons/core';
-import { lucidePlus, lucidePlusCircle } from '@ng-icons/lucide';
+import {
+    lucideAlertTriangle,
+    lucidePlus,
+    lucidePlusCircle,
+} from '@ng-icons/lucide';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import {
     HlmDialogComponent,
@@ -29,6 +41,13 @@ import { Budget, Expense } from 'utils/schemas/schemas';
 import { dateToReadableText, getCurrentMonthandYear } from 'utils/utils/utils';
 import { CHART_COLOR_SURPLUS } from '../../shared/constants';
 import { ExpenseCategorySummaryComponent } from './expense-category-summary/expense-category-summary.component';
+import {
+    HlmAlertDescriptionDirective,
+    HlmAlertDirective,
+    HlmAlertIconDirective,
+    HlmAlertTitleDirective,
+} from '@spartan-ng/ui-alert-helm';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-dashboard',
@@ -43,12 +62,20 @@ import { ExpenseCategorySummaryComponent } from './expense-category-summary/expe
         BrnDialogContentDirective,
         BrnDialogTriggerDirective,
         ExpenseCategorySummaryComponent,
+        HlmAlertDescriptionDirective,
+        HlmAlertDirective,
+        HlmAlertIconDirective,
+        RouterModule,
     ],
-    providers: [provideIcons({ lucidePlus, lucidePlusCircle })],
+    providers: [
+        provideIcons({ lucidePlus, lucidePlusCircle, lucideAlertTriangle }),
+    ],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
+    @ViewChild('budgetSummary') chartCanvasRef!: ElementRef;
+
     budgetService = inject(BudgetService);
     themeService = inject(ThemeService);
     theme = this.themeService.getTheme();
@@ -56,6 +83,7 @@ export class DashboardComponent implements OnInit {
     expenses: Expense[] = [];
     month = getCurrentMonthandYear();
     monthasReadableText = dateToReadableText(this.month);
+    budgetExists = signal(true);
 
     constructor() {
         effect(() => {
@@ -75,68 +103,62 @@ export class DashboardComponent implements OnInit {
     async ngOnInit(): Promise<void> {
         try {
             const budget = await this.budgetService.getCurrentBudget();
-
             if (!budget) {
                 console.log('no budget set. Please set it');
-                // todo: set banner informing user they need to set a budget.
+                this.budgetExists.set(false);
                 return;
             }
-
+            this.budgetExists.set(true);
             const income = budget.income;
             this.expenses = budget.expenses;
-
             const surplus =
                 income -
                 this.expenses.reduce((total, current) => {
                     return total + current.amount;
                 }, 0);
-
-            this.summaryChart = new Chart(
-                document.getElementById('budgetSummary') as ChartItem,
-                {
-                    type: 'doughnut',
-                    data: {
-                        labels: [
-                            ...this.expenses.map(
-                                (expense) =>
-                                    `${toTitleCase(expense.name)}: $${expense.amount}`
-                            ),
-                            `Surplus: $${surplus}`,
-                        ],
-                        datasets: [
-                            {
-                                data: [
-                                    ...this.expenses.map(
-                                        (expense) => expense.amount
-                                    ),
-                                    surplus,
-                                ],
-                                backgroundColor: [
-                                    ...this.expenses.map((expense, i) =>
-                                        getColorsForSummaryChart(i)
-                                    ),
-                                    CHART_COLOR_SURPLUS,
-                                ],
-                                borderColor: '#1c1b22',
-                                hoverOffset: 4,
-                            },
-                        ],
-                    },
-                    options: {
-                        plugins: {
-                            legend: {
-                                position: 'bottom',
-                                align: 'center',
-                                labels: {
-                                    padding: 25,
-                                    color: setLabelColor(this.theme()),
-                                    usePointStyle: true,
-                                },
+            this.summaryChart = new Chart(this.chartCanvasRef.nativeElement, {
+                type: 'doughnut',
+                data: {
+                    labels: [
+                        ...this.expenses.map(
+                            (expense) =>
+                                `${toTitleCase(expense.name)}: $${expense.amount}`
+                        ),
+                        `Surplus: $${surplus}`,
+                    ],
+                    datasets: [
+                        {
+                            data: [
+                                ...this.expenses.map(
+                                    (expense) => expense.amount
+                                ),
+                                surplus,
+                            ],
+                            backgroundColor: [
+                                ...this.expenses.map((expense, i) =>
+                                    getColorsForSummaryChart(i)
+                                ),
+                                CHART_COLOR_SURPLUS,
+                            ],
+                            borderColor: '#1c1b22',
+                            hoverOffset: 4,
+                        },
+                    ],
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            position: 'bottom',
+                            align: 'center',
+                            labels: {
+                                padding: 25,
+                                color: setLabelColor(this.theme()),
+                                usePointStyle: true,
                             },
                         },
                     },
-                }
-            );
+                },
+            });
         } catch (error) {
             console.log(error);
         }

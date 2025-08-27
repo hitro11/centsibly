a2a0e38e685fffc40fca70396a8b387886895bc8
hitro11 <rohitnazareth07@gmail.com>
2025-08-27 12:31:34 -0400

    refactor transaction service to use observables
    - refactor transaction service to use observables instead of promises.
    - improve rendering logic for summary chart.

--- apps/client/src/app/dashboard/add-transaction/add-transaction.component.ts
+++ apps/client/src/app/dashboard/add-transaction/add-transaction.component.ts
@@ -6,6 +6,8 @@ import {
     OnDestroy,
     OnInit,
     output,
+    signal,
+    WritableSignal,
 } from '@angular/core';
 import { Expense } from 'utils/schemas/schemas';
 import { BrnSelectImports } from '@spartan-ng/ui-select-brain';
@@ -25,7 +27,7 @@ import { zodValidator } from '../../shared/validators';
 import { TransactionSchema } from '@centsibly/utils/schemas';
 import { provideIcons } from '@ng-icons/core';
 import { lucideCheckCircle } from '@ng-icons/lucide';
-import { timer } from 'rxjs';
+import { catchError, EMPTY, of, Subject, takeUntil, tap, timer } from 'rxjs';
 import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
 
 @Component({
@@ -53,12 +55,13 @@ export class AddTransactionComponent implements OnInit, OnDestroy {
 
     fb = inject(FormBuilder);
     transactionService = inject(TransactionService);
-    submissionStatus: 'none' | 'loading' | 'submitted' | 'submittedWithError' =
-        'none';
+    submissionStatus: WritableSignal<
+        'none' | 'loading' | 'submitted' | 'submittedWithError'
+    > = signal('none');
+    private destroy$ = new Subject<void>();
+
     submissionError = '';
     transactionPosted = false;
-
-    ngOnInit(): void {}
 
     form = this.fb.group({
         type: [
@@ -69,6 +72,8 @@ export class AddTransactionComponent implements OnInit, OnDestroy {
         amount: [null, [zodValidator(TransactionSchema.shape.amount)]],
     });
 
+    ngOnInit(): void {}
+
     toTitleCase(text: string) {
         return toTitleCase(text);
     }
@@ -77,44 +82,69 @@ export class AddTransactionComponent implements OnInit, OnDestroy {
         this.form.controls[formControl].updateValueAndValidity();
     }
 
-    async onSubmit(): Promise<void> {
+    onSubmit() {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+
+        let parsedData = null;
+
         try {
             const formValues = this.form.getRawValue();
-            const result = TransactionSchema.safeParse(formValues);
-
-            if (!result.success) {
-                console.error('input not valid: ', result.error);
-                return;
-            } else if (result.success) {
-                this.submissionStatus = 'loading';
-                await this.transactionService.postTransactions(result.data);
-                this.submissionStatus = 'submitted';
-                this.transactionPosted = true;
-
-                this.form.reset(
-                    {
-                        type: 'expense',
-                        category: null,
-                        amount: null,
-                    },
-                    {
-                        emitEvent: true,
-                        onlySelf: true,
-                    }
-                );
-
-                this.form.controls['category'].setErrors(null);
-                this.form.controls['amount'].setErrors(null);
-
-                timer(2000).subscribe(() => {
-                    this.submissionStatus = 'none';
-                });
-            }
-        } catch (error) {
-            this.submissionStatus = 'submittedWithError';
-            this.submissionError = JSON.stringify(error);
-            console.error(error);
+            parsedData = TransactionSchema.parse(formValues);
+        } catch (err) {
+            this.handleValidationError(err);
+            return;
         }
+
+        this.submissionStatus.set('loading');
+
+        this.transactionService
+            .postTransactions(parsedData)
+            .pipe(
+                catchError((err) => {
+                    this.handleSubmissionError(err);
+                    return EMPTY;
+                }),
+                takeUntil(this.destroy$)
+            )
+            .subscribe((resp) => {
+                if (resp) {
+                    this.submissionStatus.set('submitted');
+                    this.transactionPosted = true;
+
+                    this.form.reset(
+                        {
+                            type: 'expense',
+                            category: null,
+                            amount: null,
+                        },
+                        {
+                            emitEvent: true,
+                            onlySelf: true,
+                        }
+                    );
+
+                    this.form.controls['category'].setErrors(null);
+                    this.form.controls['amount'].setErrors(null);
+
+                    timer(2000).subscribe(() => {
+                        this.submissionStatus.set('none');
+                    });
+                }
+            });
+    }
+
+    private handleValidationError(err: unknown) {
+        console.error(err);
+    }
+
+    private handleSubmissionError(err: Error) {
+        this.submissionStatus.set('submittedWithError');
+        this.submissionError = JSON.stringify(err);
+        console.error(err);
+        // todo: toast notif
     }
 
     ngOnDestroy(): void {
@@ -123,5 +153,8 @@ export class AddTransactionComponent implements OnInit, OnDestroy {
         if (this.transactionPosted) {
             this.transactionSubmitted.emit();
         }
+
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 }

ecc0396cb35630b2dbbb5cd1dd6cd16f947aafdc
hitro11 <rohitnazareth07@gmail.com>
2025-08-27 16:33:49 -0400

    revert changes

--- apps/server/src/api/middleware/error-handler.middleware.ts
+++ apps/server/src/api/middleware/error-handler.middleware.ts
@@ -8,14 +8,10 @@ export const ErrorHandler = (
     res: Response,
     next: NextFunction // eslint-disable-line
 ) => {
-    logger.error('ERROR: ');
-    logger.error(err);
+    logger.error('ERROR!!');
     const errMsg = err.message || 'Something went wrong';
     const stack = process.env.NODE_ENV === 'development' ? err.stack : {};
-
-    const eo = createHttpResponse('error', null, errMsg, stack);
-
-    logger.error('eo: ', JSON.stringify(eo));
-
-    res.status(err.statusCode || 500).json('sss');
+    res.status(err.statusCode || 500).json(
+        createHttpResponse('error', null, errMsg, stack)
+    );
 };
